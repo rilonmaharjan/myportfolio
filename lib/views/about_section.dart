15610acab01328746135f20daa022c114bf60b6c
rilonmaharjan <rilon.maharjan@gmail.com>
2025-05-07 15:41:37 +0545

    [modifeid :: about section]

--- lib/views/about_section.dart
+++ lib/views/about_section.dart
@@ -112,7 +112,7 @@ class _AboutSectionState extends State<AboutSection> {
       padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
-        color: Colors.grey.withValues(alpha:0.1),
+        color: Colors.grey.withValues(alpha:0.2),
       ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
