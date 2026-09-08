e75a28ee485203ba29a967eb87888a36ece226c1
rilonmaharjan <rilon.maharjan@gmail.com>
2026-09-08 16:29:07 +0545

    Update web build

--- lib/constant/constant.dart
+++
@@ -1,8 +0,0 @@
-String name         = "Rilon Maharjan";
-String location     = "Sankhamul, Nepal";
-String email        = "rilon.maharjan@gmail.com";
-String phone        = "+977 9861333461";
-String profession   = "Flutter Developer";
-String expereince   = "3+";
-String skills       = "Flutter, Dart, Firebase, REST APIs, Git, HTML, CSS, JavaScript";
-String cvUrl        = "https://drive.google.com/file/d/1inI9QKH8llirQjz0ewDOOxbudqu6x9Y0/view?usp=drivesdk";
