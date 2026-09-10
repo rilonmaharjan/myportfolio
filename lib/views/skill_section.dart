6d801e63fee2720fd3f37d6b3d4642375deaf807
rilonmaharjan <rilon.maharjan@gmail.com>
2026-09-10 14:51:40 +0545

    feat: add SkillsSection component with interactive category filtering and categorized technical skill cards

--- lib/views/skill_section.dart
+++ lib/views/skill_section.dart
@@ -192,6 +192,7 @@ class _SkillsSectionState extends State<SkillsSection> {
         horizontal: isMobile ? 20 : isTablet ? 40 : 100,
         vertical: isMobile ? 40 : 60,
       ),
+      width : double.infinity,
       color: Colors.grey.withValues(alpha: widget.isDarkMode ? 0.1 : 0.15),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
