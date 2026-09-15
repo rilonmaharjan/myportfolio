fbc16417a21627d099ab632441d254540b5c56bd
rilonmaharjan <rilon.maharjan@gmail.com>
2026-09-15 16:59:56 +0545

    Update Name

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Portfolio',
+      title: 'Rilon Maharjan | Portfolio',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
